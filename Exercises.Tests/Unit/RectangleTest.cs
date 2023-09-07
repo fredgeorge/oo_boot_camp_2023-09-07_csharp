aa81c2452767a1f45849e4d403c9c597380ba7fc
Fred George <fredgeorge@acm.org>
2023-09-07 14:55:41 +0530

    Fred: Rectangle with Area(), Perimeter() and range checking

--- Exercises.Tests/Unit/RectangleTest.cs
+++ Exercises.Tests/Unit/RectangleTest.cs
@@ -4,15 +4,35 @@
  * @author Fred George  fredgeorge@acm.org
  */
 
+using System;
 using Exercises.Geometry;
 using Xunit;
 
 namespace Exercises.Tests.Unit;
 
-    // Ensures that Rectangle works correctly
-    public class RectangleTest {
-            [Fact]
-            public void Area() {
-                Assert.Equal(24.0, new Rectangle(4.0, 6.0).Area());
-            }
-        }
+// Ensures that Rectangle works correctly
+public class RectangleTest
+{
+    [Fact]
+    public void Area()
+    {
+        Assert.Equal(24.0, new Rectangle(4.0, 6.0).Area());
+        Assert.Equal(30.0, new Rectangle(5.0, 6.0).Area());
+        Assert.Equal(30, new Rectangle(5, 6).Area());
+    }
+
+    [Fact]
+    public void Perimeter()
+    {
+        Assert.Equal(20.0, new Rectangle(4.0, 6.0).Perimeter());
+        Assert.Equal(22.0, new Rectangle(5.0, 6.0).Perimeter());
+        Assert.Equal(22, new Rectangle(5, 6).Perimeter());
+    }
+
+    [Fact]
+    public void ValidateParameters()
+    {
+        Assert.Throws<ArgumentException>(() => new Rectangle(0.0, 6.0));
+        Assert.Throws<ArgumentException>(() => new Rectangle(4.0, 0.0));
+    }
+}
--- Exercises/Geometry/Rectangle.cs
+++ Exercises/Geometry/Rectangle.cs
@@ -7,4 +7,17 @@
 namespace Exercises.Geometry;
 
 public class Rectangle {
+    private readonly double _length;
+    private readonly double _width;
+
+    public Rectangle(double length, double width)
+    {
+        if (length <= 0.0 || width <= 0.0) throw new ArgumentException("Each dimension must be larger than zero");
+        _length = length;
+        _width = width;
+    }
+
+    public double Area() => _length * _width;
+
+    public double Perimeter() => 2 * (_width + _length);
 }
