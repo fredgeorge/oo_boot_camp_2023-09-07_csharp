e2cb0844adfbc99e616b1400a17bc42aa55330b3
Fred George <fredgeorge@acm.org>
2023-09-12 14:49:07 +0530

    Fred: Graph with HopCount(); not necessarily minimum; duplicate code exists

--- Exercises/Graph/Node.cs
+++ Exercises/Graph/Node.cs
@@ -9,6 +9,7 @@ namespace Exercises.Graph;
 // Understands its neighbors
 public class Node
 {
+    private const int Unreachable = -1;
     private readonly List<Node> _neighbors = new();
     public Node To(Node neighbor)
     {
@@ -18,12 +19,33 @@ public class Node
 
     public bool CanReach(Node destination) => CanReach(destination, NoVisitedNodes());
 
+    public int HopCount(Node destination)
+    {
+        var result = HopCount(destination, NoVisitedNodes());
+        if (result == Unreachable) throw new ArgumentException("Destination is unreachable");
+        return result;
+    }
+
     private bool CanReach(Node destination, List<Node> visitedNodes)
     {
         if (this == destination) return true;
         if (visitedNodes.Contains(this)) return false;
         visitedNodes.Add(this);
-        return _neighbors.Any(n=>n.CanReach(destination, visitedNodes));
+        return _neighbors.Any(n => n.CanReach(destination, visitedNodes));
+    }
+
+    private int HopCount(Node destination, List<Node> visitedNodes)
+    {
+        if (this == destination) return 0;
+        if (visitedNodes.Contains(this)) return Unreachable;
+        visitedNodes.Add(this);
+        foreach (var neighbor in _neighbors)
+        {
+            var neighborHopCount = neighbor.HopCount(destination, visitedNodes);
+            if (neighborHopCount != Unreachable) return neighborHopCount + 1;
+        }
+
+        return Unreachable;
     }
 
     private List<Node> NoVisitedNodes() => new();
